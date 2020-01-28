e3c78abd29cb722a2a02c35c4420c2f6096acd11
Vanessa <vanessatellez45@gmail.com>
2020-01-28 16:59:18 -0500

    MVP day2, added session and cookie

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -20,9 +20,7 @@ router.get('/secret', (req, res, next) => {
 
 router.post('/register', (req, res) => {
     let user = req.body;
-
     const hash = bcrypt.hashSync(req.body.password, 8);
-
     user.password = hash;
 
     Users.add(user)
@@ -41,6 +39,9 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
         if (user && bcrypt.compareSync(password, user.password)){
+            req.session.loggedIn = true;
+            req.session.userId = user.id;
+
             res.status(200).json({ message: `You're Logged in! Welcome ${user.username}!`});
         } else {
             res.status(401).json({ message: 'You Shall NOT Pass!'});
@@ -49,6 +50,22 @@ router.post('/login', (req, res) => {
     .catch(error => {
         res.status(500).json(error);
     });
+});
+
+router.get('/logout', (req, res) => {
+    if (req.session){
+        req.session.destroy(err => {
+            if (err) {
+                res.status(500).json({
+                    message: "Cannot log out."
+                });
+            } else {
+                res.status(200).json({ message: "Bye. Thanks for visiting!"})
+            }
+        });
+    } else {
+        res.status(204);
+    }
 });
 
 module.exports = router;
